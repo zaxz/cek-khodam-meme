bd3cc7aa854329ca66d60184c87f8faa2e19b972
zaxz <ahmadzx3116@gmail.com>
2024-06-21 18:35:31 +0700

    update khodam

--- khodams.js
+++ khodams.js
@@ -61,8 +61,113 @@ const khodams = [
   },
   {
     id: 13,
-    nama_khodam: "Wahyu",
+    nama_khodam: "Mas Wahyu",
     gambar_khodam: "wahyu.jpg",
+  },
+  {
+    id: 14,
+    nama_khodam: "Muhammad Ibnu",
+    gambar_khodam: "muhammadibnu.jpg",
+  },
+  {
+    id: 15,
+    nama_khodam: "Masbro",
+    gambar_khodam: "masbro.jpg",
+  },
+  {
+    id: 16,
+    nama_khodam: "Joko Menggoda",
+    gambar_khodam: "menggoda.jpg",
+  },
+  {
+    id: 17,
+    nama_khodam: "Pablo Laler",
+    gambar_khodam: "pablo.jpg",
+  },
+  {
+    id: 18,
+    nama_khodam: "Pitik Pegal",
+    gambar_khodam: "pegal.jpg",
+  },
+  {
+    id: 19,
+    nama_khodam: "Riski Pajero",
+    gambar_khodam: "riskipajero.jpg",
+  },
+  {
+    id: 20,
+    nama_khodam: "Sabrina Nyamuk",
+    gambar_khodam: "sabrina.jpg",
+  },
+  {
+    id: 21,
+    nama_khodam: "Similikitiw",
+    gambar_khodam: "similikitiw.jpg",
+  },
+  {
+    id: 22,
+    nama_khodam: "Squidword Jawir",
+    gambar_khodam: "squidjawir.jpg",
+  },
+  {
+    id: 23,
+    nama_khodam: "Hiu Sucipto",
+    gambar_khodam: "sucipto.jpg",
+  },
+  {
+    id: 24,
+    nama_khodam: "Cowo Ghibli Temee",
+    gambar_khodam: "temee.jpg",
+  },
+  {
+    id: 25,
+    nama_khodam: "Yanto Kumbang",
+    gambar_khodam: "yanto.jpg",
+  },
+  {
+    id: 26,
+    nama_khodam: "Kumbang Amelia",
+    gambar_khodam: "amelia.jpg",
+  },
+  {
+    id: 27,
+    nama_khodam: "Andre Semut",
+    gambar_khodam: "andre.jpg",
+  },
+  {
+    id: 28,
+    nama_khodam: "Dani Walang Sembah",
+    gambar_khodam: "dani.jpg",
+  },
+  {
+    id: 29,
+    nama_khodam: "Daniel Surfing",
+    gambar_khodam: "daniel.jpg",
+  },
+  {
+    id: 30,
+    nama_khodam: "Fahri Cilok",
+    gambar_khodam: "fahricilok.jpg",
+  },
+  {
+    id: 31,
+    nama_khodam: "Fajar Spider",
+    gambar_khodam: "fajar.jpg",
+  },
+  {
+    id: 32,
+    nama_khodam: "Fauzan Onggo-onggo",
+    gambar_khodam: "fauzan.jpg",
+  },
+  {
+    id: 33,
+    nama_khodam: "Celeng Icikiwir",
+    gambar_khodam: "icikiwir.jpg",
+  },
+  {
+    id: 34,
+    nama_khodam: "Jaki Curut",
+    gambar_khodam: "jaki.jpg",
   },
 ];
 export default khodams;
