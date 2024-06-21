b6fb174220c2987f3fe3b7eaf6ca59d4efddfc91
zaxz <ahmadzx3116@gmail.com>
2024-06-21 15:05:49 +0700

    nande?

---
+++ khodams.js
@@ -0,0 +1,68 @@
+const khodams = [
+  {
+    id: 1,
+    nama_khodam: "Lord Ambatukam",
+    gambar_khodam: "ambatukam_mamah.jpg",
+  },
+  {
+    id: 2,
+    nama_khodam: "Lord Apabila",
+    gambar_khodam: "apabila.jpg",
+  },
+  {
+    id: 3,
+    nama_khodam: "Lord Cerdas",
+    gambar_khodam: "cerdas.jpg",
+  },
+  {
+    id: 4,
+    nama_khodam: "Sonic KSABAR",
+    gambar_khodam: "ksabar.jpg",
+  },
+  {
+    id: 5,
+    nama_khodam: "Pak Lantas",
+    gambar_khodam: "lantas.jpg",
+  },
+  {
+    id: 6,
+    nama_khodam: "LMAO Pisan Kang",
+    gambar_khodam: "lmao.jpg",
+  },
+  {
+    id: 7,
+    nama_khodam: "Anjing Mengapa",
+    gambar_khodam: "mengapa.jpg",
+  },
+  {
+    id: 8,
+    nama_khodam: "PENIS Pede dan Narsis",
+    gambar_khodam: "pededannarsis.jpg",
+  },
+  {
+    id: 9,
+    nama_khodam: "PRAGOS",
+    gambar_khodam: "pragos.jpg",
+  },
+  {
+    id: 10,
+    nama_khodam: "Si Ireng",
+    gambar_khodam: "siireng.jpg",
+  },
+  {
+    id: 11,
+    nama_khodam: "Lord Terkadang",
+    gambar_khodam: "terkadang.jpg",
+  },
+  {
+    id: 12,
+    nama_khodam: "Jancuk Pristel",
+    gambar_khodam: "jancukpristel.jpg",
+  },
+  {
+    id: 13,
+    nama_khodam: "Wahyu",
+    gambar_khodam: "wahyu.jpg",
+  },
+];
+export default khodams;
---
+++ script.js
@@ -0,0 +1,31 @@
+import khodams from "./khodams.js";
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let birthdate = document.getElementById("birthdate").value;
+  let khodamResult = cekKhodam(name, birthdate);
+  return khodamResult;
+});
+
+function cekKhodam(name, birthdate) {
+  let nameVal = name;
+  let dateVal = birthdate;
+  const container = document.getElementById("hasil");
+  const randomIndex = Math.floor(Math.random() * khodams.length);
+  const khodam = khodams[randomIndex];
+
+  container.innerHTML = `
+        <p class="text-xl ">Nama Anda adalah : </p>
+        <p class="text-xl font-bold">${nameVal}</p>
+        <p class="text-xl ">Tanggal lahir anda adalah :</p>
+        <p class="text-xl font-bold">${dateVal}</p>
+        <p class="text-xl ">Khodam Anda adalah :</p>
+        <p class="text-xl font-bold mb-3">${khodam.nama_khodam}</p>
+        <img class="max-w-[50%]" src="/assets/img/${khodam.gambar_khodam}" alt="${khodam.gambar_khodam}">
+    `;
+}
+
+// Call the function to display a random khodam when the page loads
+// window.onload = displayRandomKhodam;
